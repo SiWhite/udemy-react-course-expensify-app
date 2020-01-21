85c421b7880e401e50c8b3af5b475994695a160e
Simon <simon@Simon-iMac.local>
2020-01-21 14:48:34 +1300

    upgrading to try and fix dev-server recompiling issue

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
 
 export const ExpenseListItem = ({ id, description, amount, createdAt }) => (
     <div>
