6cf753951d3aa974afcd81c92e7f13f30b25b66b
Simon <simon@Simon-iMac.local>
2020-02-26 14:20:48 +1300

    Setup test database env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,34 +2,36 @@ import * as firebase from 'firebase/app';
 import 'firebase/database';
 
 var firebaseConfig = {
-    apiKey: "AIzaSyBtqx5_kn5s5gpT_qx9YM0zVHU9yHwYSIY",
-    authDomain: "expensify-7a288.firebaseapp.com",
-    databaseURL: "https://expensify-7a288.firebaseio.com",
-    projectId: "expensify-7a288",
-    storageBucket: "expensify-7a288.appspot.com",
-    messagingSenderId: "1095147451842",
-    appId: "1:1095147451842:web:95127588dbaece5ccbd25b",
-    measurementId: "G-QS1LYSRPH5"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId:process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 
-// child_removed
-database.ref('expenses').on('child_removed', (snapshot) => {
-    console.log(snapshot.key, snapshot.val());
-});
+export { firebase, database as default };
 
-//child_changed 
-database.ref('expenses').on('child_changed', (snapshot) => {
-    console.log(snapshot.key, snapshot.val());
-});
+// // child_removed
+// database.ref('expenses').on('child_removed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+// });
 
-//child_added
-database.ref('expenses').on('child_added', (snapshot) => {
-    console.log(snapshot.key, snapshot.val());
-});
+// //child_changed 
+// database.ref('expenses').on('child_changed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+// });
+
+// //child_added
+// database.ref('expenses').on('child_added', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+// });
 
 
 // const onExpenseChange = database.ref('expenses').on('value', (snapshot) => {
